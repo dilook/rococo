07e2b070ff21cc63650072105270ab6c5a838dd7
Denis Usoltsev <dusoltsev@oskelly.ru>
2025-06-14 22:55:53 +0400

    add: db support, work with sessions and cors

---
+++ rococo-gateway/src/main/java/guru/qa/rococo/config/SecurityConfigLocal.java
@@ -0,0 +1,48 @@
+package guru.qa.rococo.config;
+
+
+import guru.qa.rococo.service.cors.CorsCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+@Profile({"local"})
+public class SecurityConfigLocal {
+
+  private final CorsCustomizer corsCustomizer;
+
+  @Autowired
+  public SecurityConfigLocal(CorsCustomizer corsCustomizer) {
+    this.corsCustomizer = corsCustomizer;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    corsCustomizer.corsCustomizer(http);
+
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(customizer ->
+            customizer.requestMatchers(
+                    antMatcher("/api/session/current"),
+                    antMatcher("/api/artists/**"),
+                    antMatcher("/api/museum/**"),
+                    antMatcher("/api/painting/**"))
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        ).oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+    return http.build();
+  }
+}
