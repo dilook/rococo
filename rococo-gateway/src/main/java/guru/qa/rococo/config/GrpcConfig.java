fce1dcb0e0adc2513434cd7a694ae5741b8e71a7
dilook <udenav@yandex.ru>
2025-07-02 02:08:05 +0400

    add: increase max-inbound-message-size:

--- rococo-gateway/src/main/java/guru/qa/rococo/config/GrpcConfig.java
+++ rococo-gateway/src/main/java/guru/qa/rococo/config/GrpcConfig.java
@@ -1,8 +1,9 @@
 package guru.qa.rococo.config;
 
-import guru.qa.rococo.grpc.RococoMuseumServiceGrpc;
 import guru.qa.rococo.grpc.RococoCountryServiceGrpc;
+import guru.qa.rococo.grpc.RococoMuseumServiceGrpc;
 import guru.qa.rococo.service.GrpcClientLoggerInterceptor;
+import io.grpc.Channel;
 import io.grpc.ClientInterceptors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,27 +12,28 @@ import org.springframework.grpc.client.GrpcChannelFactory;
 @Configuration
 public class GrpcConfig {
 
+    private static final String MUSEUM_CHANNEL_NAME = "rococo-museum";
+
     @Bean
     public RococoMuseumServiceGrpc.RococoMuseumServiceBlockingStub museumServiceStub(
             GrpcChannelFactory channels,
             GrpcClientLoggerInterceptor loggerInterceptor) {
-        return RococoMuseumServiceGrpc.newBlockingStub(
-                ClientInterceptors.intercept(
-                        channels.createChannel("rococo-museum"),
-                        loggerInterceptor
-                )
-        );
+        Channel interceptedChannel = createInterceptedChannel(channels, loggerInterceptor, MUSEUM_CHANNEL_NAME);
+        return RococoMuseumServiceGrpc.newBlockingStub(interceptedChannel);
     }
 
     @Bean
     public RococoCountryServiceGrpc.RococoCountryServiceBlockingStub countryServiceStub(
             GrpcChannelFactory channels,
             GrpcClientLoggerInterceptor loggerInterceptor) {
-        return RococoCountryServiceGrpc.newBlockingStub(
-                ClientInterceptors.intercept(
-                        channels.createChannel("rococo-museum"),
-                        loggerInterceptor
-                )
+        Channel interceptedChannel = createInterceptedChannel(channels, loggerInterceptor, MUSEUM_CHANNEL_NAME);
+        return RococoCountryServiceGrpc.newBlockingStub(interceptedChannel);
+    }
+
+    private Channel createInterceptedChannel(GrpcChannelFactory channels, GrpcClientLoggerInterceptor interceptor, String channelName) {
+        return ClientInterceptors.intercept(
+                channels.createChannel(channelName),
+                interceptor
         );
     }
 }
